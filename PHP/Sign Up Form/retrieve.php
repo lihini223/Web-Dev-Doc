c6cf64b0dbb696225ebd4ebab4ccc9008c3c237d
Lihini Nisansala <lihininisansala95@gmail.com>
2020-07-27 22:34:06 +0530

    update

--- PHP/Sign Up Form/retrieve.php
+++ PHP/Sign Up Form/retrieve.php
@@ -7,5 +7,9 @@
 
     $conn = mysqli_connect($servername,$username,$password,$dbname);
 
+    if(!$conn){
+        //echo("Connection Failure!");
+        die("Connection Failed".mysqli_connect_error());
+    }
 
 ?>
