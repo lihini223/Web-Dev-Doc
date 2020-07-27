6b97ce25f1889f497cc1fcad7ef9b030b0e9a490
Lihini Nisansala <lihininisansala95@gmail.com>
2020-07-27 22:29:37 +0530

    update

--- PHP/Sign Up Form/delete.php
+++ PHP/Sign Up Form/delete.php
@@ -8,4 +8,9 @@
     $conn = mysqli_connect($servername,$username,$password,$dbname);
 
 
+    if(!$conn){
+        //echo("Connection Failure!");
+        die("Connection Failed".mysqli_connect_error());
+    }
+
 ?>
